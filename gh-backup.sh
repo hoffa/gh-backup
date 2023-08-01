74f3a21a7952bd90bb875bb4058314a202ab97d7
Christoffer Rehn <1280602+hoffa@users.noreply.github.com>
2023-08-01 11:52:37 +0300

    Backup repos

--- gh-backup.sh
+++ gh-backup.sh
@@ -1,18 +1,31 @@
 #!/bin/sh
 set -eux
 
-username=$(gh api user | jq -r .login)
-
+username=$(gh api user --jq .login)
 dirname=gh-backup-${username}-$(date -u +"%Y%m%dT%H%M%SZ")
 
 mkdir "${dirname}"
 cd "${dirname}"
 
-mkdir repos
+# Clone gists
 mkdir gists
-
+cd gists
 gh gist list | while read -r gist; do
-    id=$(echo "${gist}" | cut -f 1)
-    filename=$(echo "${gist}" | cut -f 2)
-    gh gist clone "${id}" "gists/${id}"
+	id=$(echo "${gist}" | cut -f 1)
+	gh gist clone "${id}"
 done
+cd ..
+
+# Clone repos
+mkdir repos
+cd repos
+gh repo list | while read -r repo; do
+	name=$(echo "${repo}" | cut -f 1)
+	gh repo clone "${name}" -- --depth 1
+done
+cd ..
+
+# Create archive
+cd ..
+tar czvf "${dirname}.tar.gz" "${dirname}"
+rm -rf "${dirname}"
