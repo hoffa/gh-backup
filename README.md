056fac60020e5d5467429ff23dc5b3960e2309e6
Christoffer Rehn <1280602+hoffa@users.noreply.github.com>
2025-12-05 10:51:12 +0900

    Also backup orgs you belong to

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # gh-backup
 
-Zero-config [script](gh-backup.sh) to backup GitHub repos and gists.
+Zero-config [script](gh-backup.sh) to backup your GitHub repos, gists, and repos of organizations that you belong to.
 
 Requires [GitHub CLI](https://cli.github.com).
 
 ## Usage
 
 ```bash
-curl https://raw.githubusercontent.com/hoffa/gh-backup/main/gh-backup.sh | sh 
+curl https://raw.githubusercontent.com/hoffa/gh-backup/main/gh-backup.sh | sh
 ```
 
 Which will create an archive in the form `github-backup-<username>-<timestamp>.tar.gz`.
--- gh-backup.sh
+++ gh-backup.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 set -eux
 
-if ! command -v sh >/dev/null; then
+LIMIT=1000
+
+if ! command -v gh >/dev/null; then
 	echo "gh not found"
 	echo "On macOS, you can install it with Homebrew:"
 	echo "  brew install gh"
+	exit 1
 fi
 
 gh auth status || gh auth login
@@ -17,19 +20,36 @@ cd "${dirname}"
 # Clone gists
 mkdir gists
 cd gists
-gh gist list | while read -r gist; do
+gh gist list --limit "${LIMIT}" | while read -r gist; do
 	id=$(echo "${gist}" | cut -f 1)
 	gh gist clone "${id}"
 done
 cd ..
 
-# Clone repos
+# Clone user repos
 mkdir repos
 cd repos
-gh repo list | while read -r repo; do
-	name=$(echo "${repo}" | cut -f 1)
-	gh repo clone "${name}"
+mkdir -p "${username}"
+(
+	cd "${username}"
+	gh repo list "${username}" --limit "${LIMIT}" | while read -r repo; do
+		name=$(echo "${repo}" | cut -f 1)
+		gh repo clone "${name}"
+	done
+)
+
+# Clone repos for orgs the user belongs to
+gh api user/orgs --jq '.[].login' | while read -r org; do
+	mkdir -p "${org}"
+	(
+		cd "${org}"
+		gh repo list "${org}" --limit "${LIMIT}" | while read -r repo; do
+			name=$(echo "${repo}" | cut -f 1)
+			gh repo clone "${name}"
+		done
+	)
 done
+
 cd ..
 
 # Create archive
